00c1bc56b87b5cb403240393f0fd263f87c02da2
MohamedAbdelmegeed <Mohamed.Abdelhamid.1240291@CIT.hnu.edu.eg>
2026-03-17 16:57:30 +0200

    Reorder imports in Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,9 +3,9 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import SkillsSection from "@/components/SkillsSection";
-import TimelineSection from "@/components/TimelineSection";
-import ProfessionalDevelopmentSection from "@/components/ProfessionalDevelopmentSection";
 import ProjectsSection from "@/components/ProjectsSection";
+import ProfessionalDevelopmentSection from "@/components/ProfessionalDevelopmentSection";
+import TimelineSection from "@/components/TimelineSection";
 import ContactSection from "@/components/ContactSection";
 
 const Index = () => {
