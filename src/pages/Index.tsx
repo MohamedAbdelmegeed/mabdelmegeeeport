7aeb5bdce3cf3da4bd8e1def34d545846b4e1579
MohamedAbdelmegeed <Mohamed.Abdelhamid.1240291@CIT.hnu.edu.eg>
2026-03-17 16:59:11 +0200

    Replace TimelineSection with ProjectsSection

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -24,15 +24,17 @@ const Index = () => {
       <div className="max-w-4xl mx-auto px-6">
         <div className="h-px bg-gradient-to-r from-transparent via-primary/20 to-transparent" />
       </div>
-      <TimelineSection />
+      <ProjectsSection />
       <div className="max-w-4xl mx-auto px-6">
         <div className="h-px bg-gradient-to-r from-transparent via-primary/20 to-transparent" />
       </div>
+      
       <ProfessionalDevelopmentSection />
       <div className="max-w-4xl mx-auto px-6">
         <div className="h-px bg-gradient-to-r from-transparent via-primary/20 to-transparent" />
       </div>
-      <ProjectsSection />
+
+      <TimelineSection />
       <div className="max-w-4xl mx-auto px-6">
         <div className="h-px bg-gradient-to-r from-transparent via-primary/20 to-transparent" />
       </div>
